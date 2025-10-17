e1dfe550c00f8ce96babe9c86b92b9910bdab826
JingyuZhang <j.zhang.73@student.scu.edu.au>
2025-10-17 21:08:50 +0800

    Refactor app to use standalone AppComponent and new layout

--- admin-angular/src/app/app.routes.ts
+++ admin-angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+// src/app/app.routes.ts
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EventListComponent } from './components/event-list/event-list.component';
+import { EventFormComponent } from './components/event-form/event-form.component';
+import { EventEditComponent } from './components/event-edit/event-edit.component';
+
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'events', component: EventListComponent },
+  { path: 'events/new', component: EventFormComponent },
+  { path: 'events/edit/:id', component: EventEditComponent },
+  { path: '**', redirectTo: '' }
+];
