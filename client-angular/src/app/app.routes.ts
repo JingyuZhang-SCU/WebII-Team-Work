a81032254a53fc2019ef888471a0c9870bb95d82
Li Tian <1518061368@qq.com>
2025-10-17 20:54:30 +0800

    Configure page routing

--- client-angular/src/app/app.routes.ts
+++ client-angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+// src/app/app.routes.ts
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { EventDetailsComponent } from './components/event-details/event-details.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'event/:id', component: EventDetailsComponent },
+  { path: 'register/:id', component: RegistrationComponent },
+  { path: '**', redirectTo: '' }
+];
